283c5acda56c55584d44d3faf45fbc5ff9e0b8f7
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-03 18:00:00 +0300

    Initial Commit

---
+++ MachineLearningImageConfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageConfig
+//
+//  Created by Ömer Faruk Kılıçaslan on 3.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
