13c2bee2ccfe7e68721cc18874f0c30069713370
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-03 18:15:27 +0300

    ciImage conversion

--- MachineLearningImageConfig/ViewController.swift
+++ MachineLearningImageConfig/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var changeButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var chosenImage = CIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -32,10 +37,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
         
+        if let ciImage = CIImage(image: imageView.image!) {
+            chosenImage = ciImage
+        }
+        
+        recognizeImage(image: chosenImage)
         
         
     }
     
+    
+    func recognizeImage(image: CIImage) {
+        
+        
+    }
     
 }
 
